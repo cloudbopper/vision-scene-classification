29422731ec7dc9096ca6296d4a2cc97a0f1aa708
msyamkum <meenakshi.psgit@gmail.com>
2015-04-16 17:21:27 -0500

    init to code book opt

--- CodeBookOptimization.m
+++ CodeBookOptimization.m
@@ -1,9 +1,34 @@
-function [ output_args ] = CodeBookOptimization( input_args )
+function [ output_args ] = CodeBookOptimization( imageFileList, dataBaseDir, lambda, sigma )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-%Load dictionary
 
+%Load dictionary from K-means
+inFName = fullfile(dataBaseDir, sprintf('Binit_%d.mat', params.BinitSize));
+load(inFName,'Binit');
+fprintf('Loaded Binit: %d codewords\n', params.BinitSize);
+
+%Initializing to K-means dictionary
+%Binit = transpose(Binit);
+M = size(Binit, 1);
+
+%Iterating over each image
+for f = 1:length(imageFileList)
+    % input SIFT features
+    imageFName = imageFileList{f};
+    [dirN base] = fileparts(imageFName);
+    baseFName = fullfile(dirN, base);
+    inFName = fullfile(dataBaseDir, sprintf('%s%s', baseFName, featureSuffix));
+    % load sift descriptors
+    load(inFName, 'features');
+    
+    N = size(features.data,1);
+%     for i = 1:N
+%         d = zeros(1, M);
+%         for j = 1:M
+%             d(1, j) =  
+%         end
+%     end
 
 end
 
